a7d71ecf8b36592a69cf1fc08d48910f86410fa7
Søren Aagaard Bendtsen <soren@tech-logic.dk>
2022-02-20 16:11:39 +0100

    student exe

--- week3/classExe/Student.py
+++ week3/classExe/Student.py
@@ -1,7 +1,12 @@
+import random
 import Course
 import DataSheet
 
 class Student():
+   
+
+
+
     def __init__(self,name,gender,imgUrl,datasheet):
         self.name = name
         self.gender = gender
@@ -14,6 +19,17 @@ class Student():
             grades.append((g.grade))
   
         return sum(grades)/len(grades)
+
+    def generateStudents(self,amount):
+        names = ["Nils","Lars","Kresten","Peter","Anne","August"]
+        genders = ["Male","Female","Fluid","trans"]
+        students =[]
+        for s in amount:
+           
+            students.append(Student(random.choice(names),  random.choice(genders),"URL"))
+        return students
+          
+
         
 
 
@@ -24,6 +40,16 @@ class Course():
         self.teacher = teacher
         self.ETCS = ETCS
         self.grade = grade
+    
+    def generateCourse():
+        names = ["Pyton","Robot","GameDev"]
+        classrooms =[195,105,50]
+        teachers = ["Lars","Thomas"]
+        ETCSs = [5,15,30]
+        grades = [0,2,4,7,10,12]
+
+        return Course(random.choice(names),random.choice(classrooms),random.choice(teachers),random.choice(ETCSs),random.choice(grades))
+
 
 
 
@@ -38,6 +64,12 @@ class DataSheet():
             grades.append((g.name, g.grade))
         return grades
 
+    def generateDatasheet(amount):
+        list_of_courses = []
+        for c in amount:
+            list_of_courses.append(Course.generateCourse())
+
+        return DataSheet(list_of_courses)
 
 if __name__ == '__main__':
 
@@ -46,4 +78,5 @@ if __name__ == '__main__':
     sheet = DataSheet([firstCourse,secCourse]);
     student1 = Student("Alex","Fluid","URL",sheet)
     #print(student1.datasheet.get_grades_as_list())
-    print(student1.get_avg_grade())
+    #print(student1.get_avg_grade())
+    print(Course.generateCourse())
