d35868b1141b9827e3c17a3d6d6e62210931f1cb
Søren Aagaard Bendtsen <soren@tech-logic.dk>
2022-04-20 22:27:15 +0200

    mah

--- openCV/main.py
+++ openCV/main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 img = cv2.imread('bluecard.jpg')
 
@@ -8,14 +8,20 @@ img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 tmp1 = cv2.imread('blue.png',0)
 
+
+
 res = cv2.matchTemplate(img_grey,tmp1,cv2.TM_CCOEFF_NORMED)
 
 threshold = 0.8
 
 loc = np.where(res >= threshold)
 
+print(loc)
+
 
 for pt in zip(*loc[::-1]):
-    cv2.rectangle(img, pt, (pt[0] + 10, pt[1] + 10), (0,255,255), 2)
+    cv2.rectangle(img, pt, (pt[0] + 1, pt[1] + 1), (0,255,255), 2)
 
-cv2.imshow('Detected',img)
+#cv2.imshow('Detected',img)
+plt.imshow(img)
+plt.show()
