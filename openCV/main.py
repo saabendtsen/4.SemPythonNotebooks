667bd8fdd5f9a22d31d5f6ab1da69d30ccc9e76a
Søren Aagaard Bendtsen <soren@tech-logic.dk>
2022-04-25 21:51:16 +0200

    ud

--- openCV/main.py
+++ openCV/main.py
@@ -2,7 +2,13 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('bluecard.jpg')
+img = cv2.imread('bluemana.jpg',cv2.IMREAD_GRAYSCALE)
+
+plt.imshow(img)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
 img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
@@ -23,5 +29,5 @@ for pt in zip(*loc[::-1]):
     cv2.rectangle(img, pt, (pt[0] + 1, pt[1] + 1), (0,255,255), 2)
 
 #cv2.imshow('Detected',img)
-plt.imshow(img)
+
 plt.show()
